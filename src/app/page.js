6fe5217e8b5e87678b199e29aae4a6c73dab62e9
ericholdridge <eholdridge2@gmail.com>
2026-06-10 08:04:47 -0500

    Homepage update

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,14 @@
 export default function Home() {
   return (
-    <div>
+    <>
       <h1>Eric Holdridge</h1>
-    </div>
+      <p>I'm a web developer based in Iowa, USA. Currently working <a href="https://giganticdesign.com" target="_blank" rel="noopener noreferrer">@giganticdesign</a>. I've been coding for 5 years and have a strong passion working with tech.</p>
+      <p>Technologies:</p>
+      <ul>
+        <li>Vue.js</li>
+        <li>React.js</li>
+        <li>PHP</li>
+      </ul>
+    </>
   );
 }
